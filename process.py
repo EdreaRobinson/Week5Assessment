00f532c9e309219bc8e88c048e2108f81103fbec
Edrea Robinson <edrea1979@gmail.com>
2021-11-12 16:30:08 -0600

    Part 1: Data Manipulation with Python completed

--- process.py
+++ process.py
@@ -1,12 +1,15 @@
 log_file = open("um-server-01.txt")
+# this opens the .txt file that contains the data
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): #defines the function
+    for line in log_file:    #defines the variable for the loop and initiates it
+        line = line.rstrip() #removes any white spaces at the end of the string
+        day = line[0:3]      #defines the variable as the first 3 characters of each line
+        if day == "Mon":     #tests a logical condition
+            print(line)      #if the condition is met, it returns this variable
 
 
 sales_reports(log_file)
+
+print(sales_reports)
